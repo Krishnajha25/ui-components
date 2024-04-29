e8fbd25de89c41d8dd7b68e2a5ebf988bf399e69
Krishnajha25 <krishna@torinit.ca>
2024-04-29 11:44:52 +0530

    first commit

---
+++ app/sandbox/page.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Sandbox = () => {
+  return (
+    <ul>
+      <li>
+        <Link href='/sandbox/card'>Card</Link>
+      </li>
+    </ul>
+  )
+}
+
+export default Sandbox
